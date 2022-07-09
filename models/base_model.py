e7d3b7f016fda7ec999c49934f1e07fd4a1730e0
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-07-10 01:27:46 +0300

    Task 3: update
    - Add the public methods save() and to_dic()

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Module with a class"""
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
@@ -10,25 +10,31 @@ class BaseModel:
     """
     # self stands for the instance itself
 
-    def __init__(self, id, created_at, updated_at):
-        """
-        Defines public instance attributes:
-            id: (string) That's assigned a unique id.
-            created_at: (datetime) That's assigned the current date
-                        when an instance is created.
-            updated_at: (datetime) That's assigned the the current datetime
-                        when an instance is created and it will be updated
-                        every time you change your object.
-        """
-        self.id = id
-        self.created_at = created_at
-        self.updated_at = updated_at
+    # def __init__(self, id, created_at, updated_at):
+    #     """
+    #     Defines public instance attributes:
+    #         id: (string) That's assigned a unique id.
+    #         created_at: (datetime) That's assigned the current date
+    #                     when an instance is created.
+    #         updated_at: (datetime) That's assigned the the current datetime
+    #                     when an instance is created and it will be updated
+    #                     every time you change your object.
+    #     """
+    #     self.id = id
+    #     self.created_at = created_at
+    #     self.updated_at = updated_at
 
-        id = uuid.uuid4()
+    #     id = uuid.uuid4()
 
-        # .now() picks up the current time when instance is intialised
-        created_at = datetime.datetime.now()
-        updated_at = datetime.datetime.now()
+    #     # .now() picks up the current time when instance is intialised
+    #     created_at = datetime.datetime.now()
+    #     updated_at = datetime.datetime.now()
+
+    id = uuid.uuid4()
+
+    # .now() picks up the current time when instance is intialised
+    created_at = datetime.now()
+    updated_at = datetime.now()
 
     def __str__(self):
         """Returns the information about an instance/object like:
@@ -41,6 +47,46 @@ class BaseModel:
         """self.__dict__ is a dictionary that stores
             an instances's writable attributes"""
 
-        info = print("[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__))
+        info = "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
         return info
+
+    # Pubic instance methods:
+
+    def save(self):
+        """
+        Updates the public instance attribute updated_at
+        with the current datetime when changes are done.
+        """
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values of __dict__
+        of the instance. Plus the class name, created_at and updated_at
+        as string object in ISO format.
+        """
+
+        dic = {}
+        # we need everything of __dic__ in dic, so we just assign it to dic
+        dic = self.__dict__
+
+        # then we add the rest info
+        # note: keys have to be strings
+        dic['__class__'] = self.__class__.__name__
+
+        # add id after changing to str
+        id = str(self.id)
+        dic['id'] = id
+
+        # change created_at & updated_at to ISO format
+
+        created_at = str(self.created_at.isoformat())
+        updated_at = str(self.updated_at.isoformat())
+
+        # add them to dic plus the id
+        dic['created_at'] = created_at
+        dic['updated_at'] = updated_at
+
+        return dic
